d735448e77c5bae4dd785f18cf5d05f49994b3d9
Macintosh HD <530002703@qq.com>
2020-05-09 09:18:59 +0800

    selelct编写

--- www/orm.py
+++ www/orm.py
@@ -48,6 +48,57 @@ async def execute(sql, args):
             raise
         return affected
 
+
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s,%s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class ModelMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table:%s)' % name,tableName)
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(k,Field):
+                logging.info('found mapping: %s==>%s' % (k,v))
+                mappings[k] = v
+            if v.primary_key:
+                if primaryKey:
+                    raise RuntimeError('Duplicate primary key for field: %s' % k)
+                primaryKey = k
+            else:
+                fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields
+
+        attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey,','.join(escaped_fields),tableName)
+        attrs['__insert__']
+
+
+
 class Model(dict, metaclass=ModelMetaclass):
 
     def __init__(self, **kw):
